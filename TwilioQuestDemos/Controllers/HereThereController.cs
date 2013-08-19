5f639f7e488e5f21633197dcf89bb7425778b209
Devin Rader <devin@twilio.com>
2013-08-19 15:49:05 -0400

    HereThere Warm Handoff Sample

--- TwilioQuestDemos/Controllers/HereThereController.cs
+++ TwilioQuestDemos/Controllers/HereThereController.cs
@@ -18,29 +18,87 @@ namespace TwilioQuestDemos.Controllers
             return View();
         }
 
-        public ActionResult PlaceIntoConference(string CallSid)
+        public ActionResult Here(string CallSid)
         {
             var response = new TwilioResponse();
-            response.Say("Please while while we conjure an agent");
-            response.DialConference(CallSid);
+            response.Say("You've arrived here.  And now some music.");
+            response.Play("", new { loop = "0" });
+
             return TwiML(response);
         }
 
-        public ActionResult RedirectOutOfConference(string CallSid)
+        public ActionResult RedirectFromHere(string From)
         {
             var client = new TwilioRestClient(Credentials.AccountSid, Credentials.AuthToken);
 
-            client.RedirectCall(CallSid, new CallOptions() { Url = Url.Action("Here", "There", null, "http") });
+            //lookup the live call by phone number
+            var result = client.ListCalls(new CallListRequest() { From = "", Status="in-progress" });
 
-            return new EmptyResult();
+            if (result.RestException == null)
+            {
+                var call = result.Calls.FirstOrDefault();
+
+                if (call == null)
+                {
+
+                }
+                else
+                {
+                    client.RedirectCall(call.Sid, new CallOptions() { Url = Url.Action("HereThere", "There", null, "http") });
+                }
+            }
+
+            return Json(true);
         }
 
         public ActionResult There()
         {
             var response = new TwilioResponse();
-            response.Say("You made it there!");
+            response.Say("You made it there!  Thanks for playing.");
             return TwiML(response);
+
         }
+
+        //public ActionResult Phone()
+        //{
+        //    return View();
+        //}
+
+        //public ActionResult CallerAConference(string CallSid)
+        //{
+        //    var response = new TwilioResponse();
+        //    response.Say("Hold on while we conjure an agent");
+        //    response.DialConference(CallSid);
+
+        //    var client = new TwilioRestClient(Credentials.AccountSid, Credentials.AuthToken);
+
+
+        //    return TwiML(response);
+        //}
+
+        //public ActionResult CallerBConference(string CallSid)
+        //{
+        //    var response = new TwilioResponse();
+        //    response.Say("Hold on while we conjure an agent");
+        //    response.DialConference(CallSid);
+        //    return TwiML(response);
+        //}
+
+        //public ActionResult RedirectOutOfConference(string CallSid)
+        //{
+        //    var client = new TwilioRestClient(Credentials.AccountSid, Credentials.AuthToken);
+
+        //    client.RedirectCall(CallSid, new CallOptions() { Url = Url.Action("Here", "There", null, "http") });
+
+        //    return new EmptyResult();
+        //}
+
+        //public ActionResult There()
+        //{
+        //    var response = new TwilioResponse();
+        //    response.Say("You made it there!");
+        //    return TwiML(response);
+        //}
 
     }
 }
