4f033dc4e2c0ab34cafa26bd23fe449ba43394b0
Devin Rader <devin@twilio.com>
2013-08-19 19:02:31 -0400

    Cleaned up stuff.  Added jquery & bootstrap

---
+++ TwilioQuestDemos/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace TwilioQuestDemos
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                    "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                    "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                    "~/Content/bootstrap/bootstrap.css",
+                    "~/Content/bootstrap/bootstrap-theme.css"
+            ));
+        }
+    }
+}
