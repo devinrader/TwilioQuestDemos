320e10b1edf372e73e2aea3f07623bad544699e1
Devin Rader <devin@twilio.com>
2013-08-23 13:43:05 -0400

    Fixing CSS bundling issue, maybe

--- TwilioQuestDemos/App_Start/BundleConfig.cs
+++ TwilioQuestDemos/App_Start/BundleConfig.cs
@@ -47,12 +47,12 @@ namespace TwilioQuestDemos
                     "~/Content/themes/base/jquery.ui.progressbar.css",
                     "~/Content/themes/base/jquery.ui.theme.css"));
 
-            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+            bundles.Add(new StyleBundle("~/Content/bootstrap/css").Include(
                     "~/Content/bootstrap/bootstrap.css",
                     "~/Content/bootstrap/bootstrap-theme.css"
             ));
 
-            bundles.Add(new StyleBundle("~/Content/infragistics").Include(
+            bundles.Add(new StyleBundle("~/Content/infragistics/css").Include(
                     "~/Content/Infragistics/css/themes/infragistics/infragistics.theme.css",
                     "~/Content/Infragistics/css/themes/structure/infragistics.css"));
 
